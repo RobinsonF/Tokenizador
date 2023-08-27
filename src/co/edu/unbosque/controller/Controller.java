a9a15a8f9be403a25971fcff4a2827bfb643184f
DavidMillan45 <89211677+DavidMillan45@users.noreply.github.com>
2023-08-27 18:55:35 -0500

    Update Controller.java

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,16 +1,26 @@
 package co.edu.unbosque.controller;
 
 import co.edu.unbosque.model.Model;
+import co.edu.unbosque.model.persistence.ReadFileRule;
 import co.edu.unbosque.view.View;
 
 public class Controller {
 	
 	private View view;
 	private Model model;
+	private ReadFileRule per;
 	
 	public Controller() {
 		view = new View(this);
 		model = new Model();
+		per = new ReadFileRule();
+		funcionar ();
 	}
+	
+	public void funcionar () {
+		per.readRules();
+			
+	}
+	
 
 }
