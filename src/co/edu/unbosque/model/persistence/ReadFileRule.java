e7ec7e671dca3c0cec380995990976296034204c
DavidMillan45 <89211677+DavidMillan45@users.noreply.github.com>
2023-08-27 18:59:04 -0500

    Update ReadFileRule.java

--- src/co/edu/unbosque/model/persistence/ReadFileRule.java
+++ src/co/edu/unbosque/model/persistence/ReadFileRule.java
@@ -1,5 +1,28 @@
 package co.edu.unbosque.model.persistence;
 
-public class ReadFileRule {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class ReadFileRule {
+	
+	public static void readRules() {
+		 String filePath = "C:\\Users\\Usuario\\Documents\\septimo\\compiladores\\ejemplo.txt"; 
+		 String line = "";
+	        
+	        try {
+	            File file = new File(filePath);
+	            Scanner scanner = new Scanner(file);
+	            
+	            while (scanner.hasNextLine()) {
+	                line = line + scanner.nextLine()+ "\n";
+	                
+	            }
+	            System.out.println(line);
+	            
+	            scanner.close();
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        }
+	}
 }
