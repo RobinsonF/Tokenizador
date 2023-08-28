a08cc6819cc4765cb48028f27648a589b62c22ac
DavidMillan45 <89211677+DavidMillan45@users.noreply.github.com>
2023-08-27 23:57:48 -0500

    Update TokenInfo.java

--- src/co/edu/unbosque/model/TokenInfo.java
+++ src/co/edu/unbosque/model/TokenInfo.java
@@ -1,15 +1,64 @@
 package co.edu.unbosque.model;
 
+import java.util.LinkedList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TokenInfo {
+public class Tokenizer {
 
-    public final Pattern regex;
-    public final int token;
+    private LinkedList<TokenInfo> tokenInfos;
+    private LinkedList<Token> tokens;
 
-    public TokenInfo(Pattern regex, int token) {
+    public Tokenizer() {
         super();
-        this.regex = regex;
-        this.token = token;
+        tokenInfos = new LinkedList<TokenInfo>();
+        tokens = new LinkedList<Token>();
     }
+
+    public void add(String regex, int token) {
+    	//System.out.println(regex);
+        tokenInfos.add(new TokenInfo(Pattern.compile("^(" + regex + ")"), token));
+        System.out.println("tpkañ");
+
+    }
+
+    public void tokenize(String str) {
+        String s = str.trim();
+        int totalLength = s.length();
+        tokens.clear();
+        while (!s.equals("")) {
+            System.out.println(s);
+            int remaining = s.length();
+            boolean match = false;
+            System.out.println("en");
+            for (TokenInfo info : tokenInfos) {
+                System.out.println("e4n");
+            	System.out.println(info.toString()+"i");
+                Matcher m = info.regex.matcher(s);
+                System.out.println(m+"m");
+                if (m.find()) {
+                    match = true;
+                    String tok = m.group().trim();
+                    s = m.replaceFirst("").trim();
+                    tokens.add(new Token(info.token, tok, totalLength - remaining));
+                    break;
+                }
+            }
+            if (!match) {
+                throw new LexerException("Unexpected character in input: " + s);
+            }
+        }
+    }
+
+    public LinkedList<Token> getTokens() {
+        return tokens;
+    }
+
+	@Override
+	public String toString() {
+		return "Tokenizer [tokenInfos=" + tokenInfos + ", tokens=" + tokens + "]";
+	}
+    
+    
+
 }
