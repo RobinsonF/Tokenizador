47252586d13faf1f7310389f7b23f155a9b8bb94
DavidMillan45 <89211677+DavidMillan45@users.noreply.github.com>
2023-08-27 21:57:26 -0500

    Update ReadFileRule.java

--- src/co/edu/unbosque/model/persistence/ReadFileRule.java
+++ src/co/edu/unbosque/model/persistence/ReadFileRule.java
@@ -1,28 +1,39 @@
 package co.edu.unbosque.model.persistence;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.arithmeticparser.tokens.Tokenizer;
 
 public class ReadFileRule {
 	
-	public static void readRules() {
-		 String filePath = "data\\ejemplo.txt"; 
-		 String line = "";
-	        
-	        try {
-	            File file = new File(filePath);
-	            Scanner scanner = new Scanner(file);
-	            
-	            while (scanner.hasNextLine()) {
-	                line = line + scanner.nextLine()+ "\n";
-	                
-	            }
-	            System.out.println(line);
-	            
-	            scanner.close();
-	        } catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	        }
+private File file;
+
+	
+	public ReadFileRule(File fileC) {
+		this.file = fileC;
+	}
+
+	public String readFile() {
+
+		String line = "";
+		String chain = "";
+
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+			while ((line = in.readLine()) != null) {
+				String[] splitArray = line.split(";");
+				Tokenizer tokenizer = new Tokenizer();
+				tokenizer.add(splitArray[0],Integer.parseInt(splitArray[2].trim()));
+				chain += line + "\n";
+			}
+			in.close();
+		} catch (IOException e) {
+			return null;
+		}
+		return chain;
 	}
 }
